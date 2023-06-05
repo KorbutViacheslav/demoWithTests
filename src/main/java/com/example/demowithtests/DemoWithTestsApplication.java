f612e6bcbde86e44ba5cf235707ee05351d79b90
KorbutViacheslav <watchinggameslive@gmail.com>
2023-06-05 15:15:58 +0300

    update fork

--- src/main/java/com/example/demowithtests/DemoWithTestsApplication.java
+++ src/main/java/com/example/demowithtests/DemoWithTestsApplication.java
@@ -2,6 +2,7 @@ package com.example.demowithtests;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//my fork
 
 @SpringBootApplication
 public class DemoWithTestsApplication {
