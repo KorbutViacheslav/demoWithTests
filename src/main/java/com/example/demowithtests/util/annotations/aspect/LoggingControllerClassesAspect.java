c24aed8ea54e32293f494c8c01604c395238f611
Viacheslav Korbut <minowaman777@gmail.com>
2023-06-29 20:56:09 +0300

    Completed service aspect

--- src/main/java/com/example/demowithtests/util/annotations/aspect/LoggingControllerClassesAspect.java
+++ src/main/java/com/example/demowithtests/util/annotations/aspect/LoggingControllerClassesAspect.java
@@ -1,4 +1,4 @@
-package com.example.demowithtests.util.annotations;
+package com.example.demowithtests.util.annotations.aspect;
 
 import lombok.extern.log4j.Log4j2;
 import org.aspectj.lang.JoinPoint;
