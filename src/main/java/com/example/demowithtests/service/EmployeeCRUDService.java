03ff9d77075486e0450afb26083ff7145bb08c1b
Viacheslav Korbut <minowaman777@gmail.com>
2023-07-10 23:08:49 +0300

    Completed first part hw_13

--- src/main/java/com/example/demowithtests/service/EmployeeCRUDService.java
+++ src/main/java/com/example/demowithtests/service/EmployeeCRUDService.java
@@ -121,6 +121,9 @@ public class EmployeeCRUDService implements EmployeeService {
     @Override
     public Employee handPassport(Integer employeeId, Long passportId) {
         Employee employee = getById(employeeId);
+        if(employee.getEmployeePassport().getIsHanded()){
+            throw new RuntimeException("This employee have passport");
+        }
         EmployeePassport employeePassport = employeePassportService.update(passportId);
         employee.setEmployeePassport(employeePassport);
         return employeeRepository.save(employee);
